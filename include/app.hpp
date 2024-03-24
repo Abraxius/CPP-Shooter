8e96b99362fcd0e00c44d77f838c9ec96e188bfe
Alexander Jost <josti4a@t-online.de>
2024-03-24 12:29:39 +0100

    fix: update app

--- include/app.hpp
+++ include/app.hpp
@@ -19,16 +19,20 @@ using namespace gl;
 #include "input.hpp"
 #include "timer.hpp"
 // #include "audio.hpp"
+
+
+
 #include "game_objects/model.hpp"
 #include "game_objects/lights/light_point.hpp"
 #include "game_objects/camera.hpp"
+
 #include "enemy_system/enemy_system.hpp"
 #include "game_objects/player.hpp"
 #include <Jolt/Jolt.h>
 
 #include "character/weapon.hpp"
 #include "character/raycastHit.hpp"
-
+#include <list>
 
 struct App
 {
@@ -85,15 +89,12 @@ private:
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     }
-    void draw_ui() {
+    void draw_ui()
+    {
         // Performance stats
         ImGui::SetNextWindowBgAlpha(0.35f);
         ImGui::SetNextWindowPos({20, 20});
-        ImGui::Begin("FPS_Overlay", nullptr, ImGuiWindowFlags_NoDecoration
-            | ImGuiWindowFlags_NoDocking
-            | ImGuiWindowFlags_NoSavedSettings
-            | ImGuiWindowFlags_NoFocusOnAppearing
-            | ImGuiWindowFlags_NoNav);
+        ImGui::Begin("FPS_Overlay", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav);
         ImGui::Text("%.1f fps", ImGui::GetIO().Framerate);
         ImGui::Text("%.1f ms", ImGui::GetIO().DeltaTime * 1000.0f);
         ImGui::End();
@@ -102,24 +103,18 @@ private:
         ImVec2 player_window_size = {200, 150};
         ImGui::SetNextWindowPos({ImGui::GetIO().DisplaySize.x - (player_window_size.x + 20), ImGui::GetIO().DisplaySize.y - (player_window_size.y + 20)});
         ImGui::SetNextWindowSize({200, 150});
-        ImGui::Begin("Player stats", nullptr, ImGuiWindowFlags_NoDecoration
-            | ImGuiWindowFlags_NoDocking
-            | ImGuiWindowFlags_NoSavedSettings
-            | ImGuiWindowFlags_NoFocusOnAppearing
-            | ImGuiWindowFlags_NoNav);
+        ImGui::Begin("Player stats", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav);
         ImGui::Text("Player");
         ImGui::Text("%.1f HP", player.health);
+        ImGui::Text("Munition");
+        ImGui::Text("%.d / %.d", weapon.bullets, weapon.magazine);
         ImGui::End();
 
         // Gameplay info
         ImVec2 gameplay_window_size = {250, 100};
         ImGui::SetNextWindowPos({ImGui::GetIO().DisplaySize.x - (gameplay_window_size.x + 20), 20});
         ImGui::SetNextWindowSize({250, 100});
-        ImGui::Begin("Gameplay info", nullptr, ImGuiWindowFlags_NoDecoration
-            | ImGuiWindowFlags_NoDocking
-            | ImGuiWindowFlags_NoSavedSettings
-            | ImGuiWindowFlags_NoFocusOnAppearing
-            | ImGuiWindowFlags_NoNav);
+        ImGui::Begin("Gameplay info", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav);
         ImGui::Text("%d Zombies left", enemySystem.enemies.size());
         ImGui::Text("%d Zombies killed", player.zombiesKilled);
         ImGui::End();
@@ -144,9 +139,11 @@ private:
                 // bind resources to pipeline
                 lights[iLight].bind_write(face);
                 // draw models
-                for (auto& model : models) model.draw();
+                for (auto &model : models)
+                    model.draw();
                 // draw Enemys
-                for (auto& enemy : enemySystem.enemies) enemy.draw();
+                for (auto &enemy : enemySystem.enemies)
+                    enemy.draw();
                 // draw other light models
                 for (size_t i = 0; i < lights.size(); i++)
                 {
@@ -169,10 +166,13 @@ private:
             lights[iLight].bind_read(iLight, iLight + 1);
         }
         // draw models
-        for (auto& light : lights) light.draw();
-        for (auto& model : models) model.draw();
+        for (auto &light : lights)
+            light.draw();
+        for (auto &model : models)
+            model.draw();
 
-        for (auto& enemy : enemySystem.enemies) enemy.draw();
+        for (auto &enemy : enemySystem.enemies)
+            enemy.draw();
     }
 
     std::pair<float, float> getMousePosition()
@@ -215,54 +215,72 @@ private:
             SDL_SetRelativeMouseMode(!SDL_GetRelativeMouseMode());
 
         // player movement
-        if (Keys::pressed(SDL_KeyCode::SDLK_LSHIFT)) movementSpeed *= player.sprintSpeed; // sprint button
-        if (Keys::down('s')) player.move(0.0f, 0.0f, movementSpeed);
-        if (Keys::down('w')) player.move(0.0f, 0.0f, -movementSpeed);
-        if (Keys::down('d')) player.move(movementSpeed, 0.0f, 0.0f);
-        if (Keys::down('a')) player.move(-movementSpeed, 0.0f, 0.0f);
+        float movementSpeed = timer.get_delta() * player.movementSpeed;
+        if (Keys::pressed(SDL_KeyCode::SDLK_LSHIFT))
+            movementSpeed *= player.sprintSpeed; // sprint button
+        if (Keys::down('s'))
+            player.move(0.0f, 0.0f, movementSpeed);
+        if (Keys::down('w'))
+            player.move(0.0f, 0.0f, -movementSpeed);
+        if (Keys::down('d'))
+            player.move(movementSpeed, 0.0f, 0.0f);
+        if (Keys::down('a'))
+            player.move(-movementSpeed, 0.0f, 0.0f);
 
         if (Keys::down('r'))
             weapon.reload();
 
         if (Mouse::down('1') || Keys::down('x'))
         {
-            Ray ray = raycastHit.getRaycast(window, camera);   
+            Ray ray = raycastHit.getRaycast(window, camera);
 
-            if(weapon.fire()) {
-                raycastHit.isCollision(ray, sphereTest);
+            if (weapon.fire())
+            {
+                for (auto &enemie : enemySystem.enemies) {
+                    if (raycastHit.isCollision(ray, enemie.sphereCollider)) {
+                       enemie.hit(50.0f); 
+                    }
+                }
             }
-            else {
+            else
+            {
                 weapon.noFire();
             }
-        } 
+        }
 
-        if (Keys::down('l')) enemySystem.spawnEnemys();
+        if (Keys::down('l'))
+        {
+            enemySystem.spawnEnemys(); 
+        }
         // if (Keys::pressed('r')) Mix_PlayChannel(-1, audio.samples[0], 0);
-        
+
         player.rotation.x -= player.rotationSpeed * Mouse::delta().second;
         player.rotation.y -= player.rotationSpeed * Mouse::delta().first;
 
         // Sync Camera and Player
         camera.position = player.position;
         camera.rotation = player.rotation;
-
-
     }
 
-    void updateGame() {
-        for (auto& enemy : enemySystem.enemies) {
+    void updateGame()
+    {
+        for (auto &enemy : enemySystem.enemies)
+        {
             // Check if player is seen by enemy
-            if (enemy.isPlayerInSight(player.position)) {
-                std::cout << "Spieler wurde vom Zombie gesehen!" << std::endl;
+            if (enemy.isPlayerInSight(player.position))
+            {
+                //std::cout << "Spieler wurde vom Zombie gesehen!" << std::endl;
                 // Move Enemy towards Player
                 glm::vec3 direction = glm::normalize(player.position - enemy.transform.position);
                 float movementSpeed = timer.get_delta() * enemy.movementSpeed;
                 enemy.transform.position += direction * movementSpeed;
+                enemy.sphereCollider.center = enemy.transform.position;
             }
             // Check if player is hit by enemy
             float distanceToEnemy = glm::distance(player.position, enemy.transform.position);
             float collisionRadius = 2.0f;
-            if (distanceToEnemy <= collisionRadius) {
+            if (distanceToEnemy <= collisionRadius)
+            {
                 player.takeDamage(enemy.damage);
             }
         }
@@ -276,11 +294,11 @@ private:
     // render resources
     Pipeline colorPipeline = Pipeline("shaders/default.vs", "shaders/default.fs");
     Pipeline shadowPipeline = Pipeline("shaders/shadowmapping.vs", "shaders/shadowmapping.fs");
-    //Model model = Model({0, 0, 0}, {0, 0, 0}, {.01, .01, .01}, "models/ground/ground.obj");
+    // Model model = Model({0, 0, 0}, {0, 0, 0}, {.01, .01, .01}, "models/ground/ground.obj");
 
-    Player player = Player({1, 2f, 1}, {0, 0, 0}, 100.f, 2.f, 3.f, 0.001f);
-    Camera camera = Camera({1, 2f, 1}, {0, 0, 0}, window.width, window.height);
-    
+    Player player = Player({1, 2, 1}, {0, 0, 0}, 100.f, 2.f, 3.f, 0.001f);
+    Camera camera = Camera({1, 2, 1}, {0, 0, 0}, window.width, window.height);
+
     std::array<PointLight, 1> lights = {
         PointLight({10, 20, 0}, {0, 0, 0}, {1, 1, 1}, 100.0f),
         // PointLight({-4, 4, 0}, {0, 0, 0}, {1, 1, 1}, 100.0f),
@@ -298,9 +316,11 @@ private:
     Weapon weapon;
     RaycastHit raycastHit;
 
-    //ToDo: Die einzelne Sphere durch ein Array oder Liste ersetzen
-    //std::vector<std::unique_ptr<Object>> objects;
-    Sphere sphereTest = Sphere({1, 1, 1}, 1.0f);
+    // ToDo: Die einzelne Sphere durch ein Array oder Liste ersetzen
+
+   // std::list<Sphere> sphereColliders = {};
+
+    //Sphere sphereTest = Sphere({1, 1, 1}, 1.0f);
 
     //  Audio audio;
     EnemySystem enemySystem = EnemySystem(1);
